dbc273ecadce9debc6709b94da89961e679fc0d4
nathan <njgingrich@gmail.com>
2016-04-18 17:45:43 -0400

    updated refs again

---
+++ make_refs.py
@@ -0,0 +1,11 @@
+import random
+
+ram = 4096
+pagesize = 4096
+to_gen = 10
+
+while to_gen > 0:
+    print("r:", random.randint(0, 1000))
+    if ((to_gen % 10) == 0):
+        print("dump")
+    to_gen -= 1
