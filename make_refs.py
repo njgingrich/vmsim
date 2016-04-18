4d08d4d3215e756dc9a24d7565c5199e0433e2d9
nathan <njgingrich@gmail.com>
2016-04-18 17:46:33 -0400

    removed space from gen'd refs

--- make_refs.py
+++ make_refs.py
@@ -5,7 +5,7 @@ pagesize = 4096
 to_gen = 10
 
 while to_gen > 0:
-    print("r:", random.randint(0, 1000))
+    print("r:", random.randint(0, 1000), sep="")
     if ((to_gen % 10) == 0):
         print("dump")
     to_gen -= 1
